c2aec53426710a905127efd3d4153d8bad2b8373
Amith Mihiranga <lwpamihiranga@gmail.com>
2024-10-06 10:35:45 +0530

    Pass sample post content to template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Amith M',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 6, 2024',
+    },
+    {
+        'author': 'Mihiranga A',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 4, 2024',
+    },
+    {
+        'author': 'Madhavi P',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'October 1, 2024',
+    },
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
